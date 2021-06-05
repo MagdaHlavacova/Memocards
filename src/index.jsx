21ebc400e4467eb97015f0acaaba32f0944fc88f
MagdaHlavacova <magda.hlavacova@gmail.com>
2021-06-05 11:47:43 +0200

    komponenta ButtonsCard

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Header from './components/Header';
+import ButtonsCard from './components/ButtonsCard';
 import './style.css';
 
 const App = () => (
   <div className="container">
     <Header />
-    <main></main>
+    <ButtonsCard />
     <footer></footer>
   </div>
 );
