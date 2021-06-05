b2540c4001d8e2b67801377b70656183fa3f136f
Marketa KJ <mkloubkovajancikova@gmail.com>
2021-06-05 14:59:36 +0200

    Komponenta MainPage

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import Header from './components/Header';
 import './style.css';
+import Mainpage from './components/Main-page';
 import Card from './components/Card';
 
 const App = () => (
   <div className="container">
     <Card />
     <Header />
+    <Mainpage />
     <main></main>
     <footer></footer>
   </div>
