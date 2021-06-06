405755df67d6277c633d286f94733325c5c85411
Marketa KJ <mkloubkovajancikova@gmail.com>
2021-06-06 17:19:52 +0200

    Zprovozneni navodu

--- src/components/PagesNavod/index.jsx
+++ src/components/PagesNavod/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './style.css';
 
-const PagesNavod = () => {
+const PagesNavod = (props) => {
   return (
-    <div className="karta-responzivni-obal">
+    <div className="modal-kontejner">
       <div className="modal">
         <ul className="navod-text">
           <li>1. Vyberte si téma, jaké se chcete učit.</li>
@@ -11,7 +11,9 @@ const PagesNavod = () => {
           <li>3. Otáčejte kartou.</li>
           <li>4. Přejděte na další kartu.</li>
         </ul>
-        <button className="tlacitko-navod">Zavřít návod</button>
+        <button onClick={props.zavrit} className="tlacitko-navod">
+          Zavřít návod
+        </button>
       </div>
     </div>
   );
