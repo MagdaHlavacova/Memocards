14e04c71875dab8e00689f45c7a041af3f8bb75e
MagdaHlavacova <magda.hlavacova@gmail.com>
2021-06-12 11:15:26 +0200

    oprava modalu

--- src/components/Vkladani/index.jsx
+++ src/components/Vkladani/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import './style.css';
 
 const Vkladani = (props) => {
   return (
     <div className="vkladani-kontejner">
       <div className="vkladani">
-        <button className="button1">
-          Zde si bude uživatel moci vytvářet vlastní memokarty. <br />
-          Na této části budeme pracovat po skončení DA. STAY TUNED.
+        <div className="vkladani-text">
+          <p>
+            Zde si bude uživatel moci vytvářet vlastní memokarty. <br />
+            Na této části budeme pracovat po skončení DA. STAY TUNED.
+          </p>
+        </div>
+        <button onClick={props.zavrit} className="tlacitko-navod">
+          <FontAwesomeIcon icon={faTimes} />
         </button>
       </div>
     </div>
