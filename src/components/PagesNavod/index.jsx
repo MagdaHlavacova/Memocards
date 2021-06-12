1c146e038b990e2f8fd43265456151b17d5bb295
Marketa KJ <mkloubkovajancikova@gmail.com>
2021-06-12 20:30:42 +0200

    doladění

--- src/components/PagesNavod/index.jsx
+++ src/components/PagesNavod/index.jsx
@@ -9,7 +9,7 @@ const PagesNavod = (props) => {
           <li>1. Vyberte si téma, jaké se chcete učit.</li>
           <li>2. Klikněte na PROCVIČOVAT.</li>
           <li>3. Stiskněte OTOČ KARTU pro zobrazení druhé strany karty.</li>
-          <li>4. Přejděte na další kartu.</li>
+          <li>4. Stiskněte DALŠÍ KARTA pro zobrazení další karty.</li>
         </ul>
         <button onClick={props.zavrit} className="tlacitko-navod">
           Zavřít návod
