6ec062ed8c2eddae759de2ad1045df1a674fd4a6
MagdaHlavacova <magda.hlavacova@gmail.com>
2021-06-13 15:47:33 +0200

    footer

--- src/components/Main-page/index.jsx
+++ src/components/Main-page/index.jsx
@@ -19,30 +19,32 @@ const Main = () => {
   return (
     <div className="obal">
       <div className="karta-responzivni-obal">
-        <div className="obrazek">
-          <img src={brainImg} alt="ilustrace mozku" />
-        </div>
-        <div className="texty">
-          <h1>Hlubina myšlení</h1>
-          <div className="oAplikaci">
-            <p>
-              Memokarty na míru. Při jejich tvoření se látku naučíte a
-              pravidelným opakováním pak zafixujete. <br /> Dle typu paměti či
-              potřeb probírané látky můžete vkládat texty, obrázky či zvukovou
-              stopu.
-            </p>
+        <div className="footerObal">
+          <div className="obrazek">
+            <img src={brainImg} alt="ilustrace mozku" />
           </div>
+          <div className="texty">
+            <h1>Hlubina myšlení</h1>
+            <div className="oAplikaci">
+              <p>
+                Memokarty na míru. Při jejich tvoření se látku naučíte a
+                pravidelným opakováním pak zafixujete. <br /> Dle typu paměti či
+                potřeb probírané látky můžete vkládat texty, obrázky či zvukovou
+                stopu.
+              </p>
+            </div>
 
-          <div className="tlacitka-homepage">
-            <button onClick={otevrit} className="button1">
-              Vkládání
-            </button>
-            <Link to="/PagesTemaSeznam" className="button1">
-              Procvičování
-            </Link>
+            <div className="tlacitka-homepage">
+              <button onClick={otevrit} className="button1">
+                Vkládání
+              </button>
+              <Link to="/PagesTemaSeznam" className="button1">
+                Procvičování
+              </Link>
+            </div>
           </div>
+          {ukazModal ? <Vkladani zavrit={zavrit} /> : null}
         </div>
-        {ukazModal ? <Vkladani zavrit={zavrit} /> : null}
         <div className="footer">
           <Footer />
         </div>
